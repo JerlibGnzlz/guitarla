fdcfffbf4366a7a816fca6561b6bf64058d85bb7
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-18 23:16:32 -0300

    creando un evento click

--- src/Guitar.jsx
+++ src/Guitar.jsx
@@ -2,6 +2,12 @@
 export const Guitar = ({ guitar }) => {
 
     const { id, name, image, description, price } = guitar
+
+
+    const handleClick = (id) => {
+        console.log("click", id)
+    }
+
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
@@ -14,6 +20,7 @@ export const Guitar = ({ guitar }) => {
                 <button
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={() => handleClick(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
