e063569e0b66001becf0cfdfc3871f67d29b63a1
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-03-21 13:20:27 -0300

    npm run buid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
 
-import { useMemo } from "react"
+import { useMemo } from "react";
 
 export const Header = ({ cart, removeFronCart, incrementarQuantity, uncrementarQuantity, clearCart }) => {
 
     //State derivado
-    const isEmpty = useMemo(() => cart.length === 0, [cart])
-    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
+    const isEmpty = useMemo(() => cart.length === 0, [cart]);
+    const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart]);
 
     return (
         <header className="py-5 header">
@@ -14,14 +14,14 @@ export const Header = ({ cart, removeFronCart, incrementarQuantity, uncrementarQ
                 <div className="row justify-content-center justify-content-md-between">
                     <div className="col-8 col-md-3">
                         <a href="index.html">
-                            <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
+                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
                         </a>
                     </div>
                     <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
                         <div
                             className="carrito"
                         >
-                            <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
+                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                             <div id="carrito" className="bg-white p-3">
 
@@ -98,5 +98,5 @@ export const Header = ({ cart, removeFronCart, incrementarQuantity, uncrementarQ
                 </div>
             </div >
         </header >
-    )
-}
+    );
+};
